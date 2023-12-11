2d08226a3c08faa0edc7addcd1dbfca368dd2c98
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-12-11 17:40:29 -0300

    updated main

--- pruebas/01-reading-list/CrowDev/reading-list/src/main.tsx
+++ pruebas/01-reading-list/CrowDev/reading-list/src/main.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client'
+import { App } from './App'
+
 const app = document.getElementById('app')
 if (app) {
   const root = createRoot(app)
   root.render(
-    <h1>Test</h1>
+    <App />
   )
 }
