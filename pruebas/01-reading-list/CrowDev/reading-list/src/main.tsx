43b93576f12e147e27edfa41ad01a5d8932522fc
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-12-11 23:57:56 -0300

    added styles and browser router

--- pruebas/01-reading-list/CrowDev/reading-list/src/main.tsx
+++ pruebas/01-reading-list/CrowDev/reading-list/src/main.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { BrowserRouter } from 'react-router-dom'
+import './styles/index.css'
 
 const app = document.getElementById('app')
 if (app) {
   const root = createRoot(app)
   root.render(
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   )
 }
